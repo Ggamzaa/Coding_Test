38e7a3c885ecb0baa6fb569deee319e7ae91db4b
Ggamzaa <sungsu52198@gmail.com>
2025-03-21 20:32:28 +0900

    [balance_update]

--- LG/balance_4949.py
+++ LG/balance_4949.py
@@ -1,38 +1,29 @@
-import sys
-
 while True:
-    # input
-    string = input()
-    if string == "." :
+    line = input()
+    if line == '.':
+        print("Exit")
         break
 
-    # 초기화
     stack = []
-    no = False
-    # print(string)
+    is_balanced = True
 
-    # 글자 하나 하나 보기
-    for s in string:
-        if s == "[" or s == "(":
-            stack.append(s)
-        elif s == "]":
-            if len(stack) == 0:
-                no = True
+    for char in line:
+        if char == '(' or char == '[':
+            stack.append(char)
+        elif char == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                is_balanced = False
                 break
-            a = stack.pop()
-            if a != "[":
-                no = True
-                break
-        elif s == ")":
-            if len(stack) == 0:
-                no = True
-                break
-            a = stack.pop()
-            if a != "(":
-                no = True
+        elif char == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                is_balanced = False
                 break
 
-    if len(stack) != 0 or no:
-        print("no")
+    if is_balanced and not stack:
+        print('yes')
     else:
-        print("yes")
+        print('no')
