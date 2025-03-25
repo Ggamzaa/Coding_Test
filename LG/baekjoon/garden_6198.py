c2800fa77e5eeb5de4570454f00da474f48426a4
Ggamzaa <sungsu52198@gmail.com>
2025-03-25 13:44:30 +0900

    [garden_6198]

--- LG/baekjoon/garden_6198.py
+++ LG/baekjoon/garden_6198.py
@@ -7,6 +7,17 @@ buildings = [int(input()) for _ in range(N)]
 
 # flag를 둬서 자신보다 크면 true => true면 다음 빌딩은 패스 후 다시 flag = false
 
+stack = []
+answer = 0
+
+for height in buildings:
+    while stack and stack[-1] <= height:
+        stack.pop()
+    answer += len(stack)
+    stack.append(height)
+
+print(answer)
 # 하지만 이중 for문을 돌아야하나??
+
 
 
