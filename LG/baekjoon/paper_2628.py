2df0b62f90a49326b0c0a8b7dc401e5bce8681b4
Ggamzaa <sungsu52198@gmail.com>
2025-03-25 15:08:43 +0900

    [paper_2628]

---
+++ LG/baekjoon/paper_2628.py
@@ -0,0 +1,22 @@
+import sys
+
+ga , se = map(int,input().split())
+n = int(input())
+
+garo_cut = [0, ga]
+sero_cut = [0, se]
+
+for _ in range(n):
+    d, l = map(int, input().split())
+    if d == 0:
+        sero_cut.append(l)
+    else:
+        garo_cut.append(l)
+
+garo_cut.sort()
+sero_cut.sort()
+
+max_garo = max(garo_cut[i+1] - garo_cut[i] for i in range(len(garo_cut) - 1))
+max_sero = max(sero_cut[i+1] - sero_cut[i] for i in range(len(sero_cut) - 1))
+
+print(max_garo * max_sero)
