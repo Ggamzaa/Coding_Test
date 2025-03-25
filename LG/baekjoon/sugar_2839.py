c1d29f0be138f17769575859c48efad2517202f7
Ggamzaa <sungsu52198@gmail.com>
2025-03-25 16:32:23 +0900

    sugar_2839

--- LG/baekjoon/sugar_2839.py
+++ LG/baekjoon/sugar_2839.py
@@ -13,3 +13,15 @@ for i in range(n // 5, -1, -1):
         break
 
 print(result)
+
+
+# dp = [1e9] * (n + 1)  # 충분히 큰 값으로 초기화
+# dp[0] = 0  # 0kg은 0개 필요
+
+# for i in range(3, n + 1):
+#     if i >= 3:
+#         dp[i] = min(dp[i], dp[i - 3] + 1)
+#     if i >= 5:
+#         dp[i] = min(dp[i], dp[i - 5] + 1)
+
+# print(dp[n] if dp[n] != 1e9 else -1)
