1c0d58f29d11f47ce5b1ec8c5bc99e5a3f2903cb
Ggamzaa <sungsu52198@gmail.com>
2025-03-26 21:52:29 +0900

    [nm]

---
+++ LG/baekjoon/nm_15649.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m= map(int,input().split())
+
+visited = [False] * (n + 1)
+result = []
+
+def dfs(depth):
+    if depth == m:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(1, n + 1):
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            dfs(depth + 1)
+            visited[i] = False
+            result.pop()
+
+dfs(0)
