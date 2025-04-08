5be9fa7e8de8a07f4029c414b44f7535cf72cd91
Ggamzaa <sungsu52198@gmail.com>
2025-04-08 18:12:08 +0900

    [stairs]

--- LG/baekjoon/stairs_2579.py
+++ LG/baekjoon/stairs_2579.py
@@ -5,5 +5,12 @@ input = sys.stdin.readline
 n = int(input())
 list = [int(input()) for _ in range(n)]
 
-for item in list:
-    print(item)
+# for item in list:
+#     print(item)
+
+'''
+1. 1칸 아니면 2칸씩 올라갈 수 있음
+2. 연속으로 세칸은 못 올라감
+3. 마지막칸은 무조건 지나야함
+'''
+
