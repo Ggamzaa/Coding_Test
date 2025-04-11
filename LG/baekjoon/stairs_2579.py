1c236ec71e9577a7acb512fbb655f1cea780a4e2
Ggamzaa <sungsu52198@gmail.com>
2025-04-11 21:31:55 +0900

    [stairs]

--- LG/baekjoon/stairs_2579.py
+++ LG/baekjoon/stairs_2579.py
@@ -3,7 +3,7 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-list = [int(input()) for _ in range(n)]
+stair = [int(input()) for _ in range(n)]
 
 # for item in list:
 #     print(item)
@@ -14,3 +14,17 @@ list = [int(input()) for _ in range(n)]
 3. 마지막칸은 무조건 지나야함
 '''
 
+if n == 1:
+    print(stair[0])
+elif n == 2:
+    print(stair[0] + stair[1])
+else:
+    dp = [0] * n
+    dp[0] = stair[0]
+    dp[1] = stair[0] + stair[1]
+    dp[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+
+    for i in range(3, n):
+        dp[i] = max(dp[i-2] + stair[i], dp[i-3] + stair[i-1] + stair[i])
+
+    print(dp[-1])
