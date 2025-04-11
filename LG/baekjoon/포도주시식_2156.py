f0e8dcc8d1c19f5e8585526be9ba6093d539e701
Ggamzaa <sungsu52198@gmail.com>
2025-04-11 22:33:12 +0900

    [포조주시식]

--- LG/baekjoon/포도주시식_2156.py
+++ LG/baekjoon/포도주시식_2156.py
@@ -5,3 +5,18 @@ input = sys.stdin.readline
 n = int(input())
 glass = [int(input()) for _ in range(n)]
 
+if n == 1:
+    print(glass[0])
+elif n == 2:
+    print(glass[0] + glass[1])
+else:
+    dp = [0] * n
+    dp[0] = glass[0]
+    dp[1] = glass[0] + glass[1]
+    dp[2] = max(glass[0] + glass[2], glass[1] + glass[2])
+    # dp[2] = max(glass[0] + glass[1], dp[2])
+
+    for i in range(3, n):
+        dp[i] = max(dp[i-2] + glass[i], dp[i-3] + glass[i-1] + glass[i])
+
+    print(dp[-1])
