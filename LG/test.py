0aa50495b54bec0498a0982fa2d66da304fd161a
Ggamzaa <sungsu52198@gmail.com>
2025-04-12 23:09:41 +0900

    [포도주 연습]

--- LG/test.py
+++ LG/test.py
@@ -1,24 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
-heights = list(map(int,input().split()))
+wine = [int(input()) for _ in range(n)]
 
-stack = []
-result = []
+if n == 1:
+    print(wine[0])
+elif n == 2:
+    print(wine[0] + wine[1])
+else:
+    dp = [0] * n
+    dp[0] = wine[0]
+    dp[1] = wine[0] + wine[1]
+    dp[2] = max(wine[0] + wine[2], wine[1] + wine[2], dp[1])
 
-for i in range(n):
-    # 현재 탑의 높이
-    current_height = heights[i]
+    for i in range(3, n):
+        dp[i] = max(
+            dp[i-1],
+            dp[i-2] + wine[i],
+            dp[i-3] + wine[i-1] + wine[i]
+        )
 
-    # 스택에서 나보다 낮은 탑은 제거
-    while stack and stack[-1][1] < current_height:
-        stack.pop()
-    
-    # 스택이 비었으면 0, 아니면 스택의 top이 레이저가 닿는 탑
-    if not stack:
-        result.append(0)
-    else:
-        result.append(stack[-1][0] + 1)  # 인덱스는 1부터 시작하므로 +1
-
-    # 현재 탑을 스택에 넣음
-    stack.append((i, current_height))
-
-print(' '.join(map(str, result)))
+    print(dp[-1])
