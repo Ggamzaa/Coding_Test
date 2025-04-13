1aa05e2c952fecfec36316f4b7c6841f42c5f527
Ggamzaa <sungsu52198@gmail.com>
2025-04-13 23:28:52 +0900

    [포도주 연습]

--- LG/test.py
+++ LG/test.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 n = int(input())
 wine = [int(input()) for _ in range(n)]
 
+#초기값 설정
 if n == 1:
     print(wine[0])
 elif n == 2:
@@ -15,6 +16,7 @@ else:
     dp[1] = wine[0] + wine[1]
     dp[2] = max(wine[0] + wine[2], wine[1] + wine[2], dp[1])
 
+    # 
     for i in range(3, n):
         dp[i] = max(
             dp[i-1],
